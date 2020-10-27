f9a4326fdf8133d743a162197b69bd289a6368a9
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-10-27 12:48:30 +0100

    Create Naloga2.c
    Add second homework exercise.

---
+++ Exercises/20.10/Homework/Naloga2.c
@@ -0,0 +1,74 @@
+// Funkcije open, close; kostante, ki jih uporaibmo pri open(): O_RDONLY, O_WRONLY, O_RDWR; O_APPEND, O_CREAT, O_ECL, O_TRUNC, O_SYNC
+#include <fcntl.h>
+// veliko unix sistemskih klicev: read, write, lčseek, fork, chdir, chown, ...
+#include <unistd.h>
+// Spremeljivke stdin, stdout, stderr; funkcije: gets, getc, scanf, perror, printf, fprintf
+#include <stdio.h>
+#include <errno.h>
+// funkcije: exit, malloc, atoi, rand, ...
+#include <stdlib.h>
+
+/*
+Naloga 2
+-Napišite svojo verzijo terminalskega ukaza cat s pomočjo read , write , open in close .
+-Program naj na standardni izhod (stdout ) izpiše vsebino datotek podanih preko argumentov ukazne vrstice.
+-Če je program klican brez argumentov, kopira stdin v stdout
+-Lahko je podanih tudi več vhodnih argumentov (datotek). 
+
+ */
+
+#define BUFFSIZE 512
+
+void main(int argc, char *argv[])
+{
+    int fd;
+    int bytes_read, bytes_written;
+    char buf[BUFFSIZE];
+
+    if (argc > 1)
+    {
+        // Read files.
+        for (int i = 0; i < argc; i++)
+        {
+            // Open source file
+            if ((fd = open(argv[i], O_RDONLY)) == -1)
+            {
+                perror("Izvorna datoteka (argument 1)");
+                exit(1);
+            }
+
+            // Read data
+            if ((bytes_read = read(fd, buf, BUFFSIZE)) < 0)
+            {
+                perror("read");
+                exit(1);
+            }
+                
+
+            // Write data
+            // printf(buf);
+            if ((bytes_written = write(STDOUT_FILENO, buf, bytes_read)) != bytes_read)
+            {
+                perror("write");
+                exit(1);
+            }
+            
+
+            close(fd);
+        }
+        
+        
+    }
+    else
+    {
+        // Read user input.
+        while ( (bytes_read = read(STDIN_FILENO, buf, BUFFSIZE) ) > 0) {
+            if ( write(STDOUT_FILENO, buf, bytes_read) != bytes_read ) {
+                perror("write");
+                exit(1);
+            }
+
+        }
+    }
+
+}
