2d7dad3a5dac4cc7e21afe2985fed29f9339eb3d
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-09 21:19:50 +0100

    Add missing files.

--- Lectures/9.11.2020/pipe1.c
+++ Lectures/9.11.2020/pipe1.c
@@ -1,39 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define err_sys (mess) { fprintf(stderr,"Error: %s.", mess); exit(1);
+#define err_sys(mess) { fprintf(stderr,"Error: %s.", mess); exit(1); }
 #define MAXLINE 512
 
 int main(void)
 {
     int n, fd[2];
-    pid_t
-    pid;
-    char
-    line[MAXLINE];
-    if (
-    pipe (fd) <
-    err_sys
-    ("pipe
-    if ( (pid =
-    fork ()) <
-    err_sys
-    ("fork
-    else if (pid > 0) {
-    starš
-    close
-    (fd[
-    write
-    (fd[1], "hello world n", 12);
+    pid_t pid;
+    char line[MAXLINE];
+    if (pipe (fd) < 0)
+        err_sys("pipe error");
+    
+    
+    if ( (pid = fork ()) < 0 )
+        err_sys("fork error")
+    else if (pid > 0) { //starš
+        close(fd[0]);   // Zapre bralni konec
+        write(fd[1], "hello world \n", 12);
     }
-    else {
-    otrok
-    close
-    (fd[
-    n =
-    read (fd[0], line,
-    write
-    (STDOUT_FILENO, line,
+    else {      // otrok
+        close(fd[1]);
+        n = read (fd[0], line, MAXLINE);
+        // write(STDOUT_FILENO, line, n);
     }
     exit(0);
 }
