7b3cbb6d76851011bc84ec9a3d77d71e5c434b4b
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-11-24 16:19:19 +0100

    Add latest lectures.

--- Lectures/23.11.2020/vec_niti.c
+++ Lectures/23.11.2020/vec_niti.c
@@ -7,10 +7,11 @@
 
 void *funkcija1( void *argument)
 {
-    printf("Pozdrav od niti!\n");
-    // return NULL;
+    printf("Pozdrav od niti %ld!\n", (long) argument);
+    return NULL;
 
-    return ((void *)1);     // Pravieln pristop, ampak dela tud brez castanja
+    // return ((void *)1);     // Pravieln pristop, ampak dela tud brez castanja
+
 }
 
 int main()
@@ -21,26 +22,28 @@ int main()
     for (int i = 0; i < N_thr; i++)
     {
         // Poiter na funckijo je vedno kar ime funckije.
-        if ( pthread_create( &tid[i], NULL, funkcija1, NULL) ) {
+        if ( pthread_create( &tid[i], NULL, funkcija1, (void *)i) ) {
             printf("Napaka: pthread_create");
             exit(1);
         }
     }
     
-
-    
-
     // Joinamo, da bo pocakal.
     // if ( pthread_join(tid, NULL) ) // ce nas ne zanima izh. status
     // Naslov od pointerja je dvojni pointer?
-    if ( pthread_join(tid, &ret_status) )
-    {
-        printf("Napaka: pthread_join");
-        exit(1);
-        
-        
+    for (int i = 0; i < N_thr; i++){
+        if ( pthread_join(tid[i], NULL) )
+        {
+            printf("Napaka: pthread_join");
+            exit(1);
+            
+            
 
+        }
     }
+
+   
+    
 
 
     printf("Izhodni status: %ld.\n", (long)ret_status);
